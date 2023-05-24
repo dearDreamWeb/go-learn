f96d0e115f5b64dab784aca241ce65ab482955ed
wxb <superwang9527@163.com>
2023-05-24 23:39:58 +0800

    feat: add some fields about updateAt and createAt

--- controller/v1/user.go
+++ controller/v1/user.go
@@ -6,13 +6,17 @@ import (
 	"go-test/model"
 	"go-test/utils"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
+	"time"
 )
 
 type Person struct {
-	Id       string `bson:"_id"`
-	Name     string
-	Password string
+	Id        primitive.ObjectID `bson:"_id"`
+	Name      string
+	Password  string
+	CreatedAt time.Time `bson:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at"`
 }
 
 // UserLogin 导出的方法（或函数、变量）需要使用大写字母开头的大驼峰写法来表示
@@ -40,7 +44,7 @@ func UserLogin(c *gin.Context) {
 			"msg":     "用户名或密码错误",
 		})
 	} else {
-		token, _ := utils.CreateToken(result.Id)
+		token, _ := utils.CreateToken(result.Id.Hex())
 		c.JSON(http.StatusOK, gin.H{
 			"success": true,
 			"msg":     "登录成功",
@@ -59,8 +63,15 @@ func UserLogin(c *gin.Context) {
 func UserRegister(c *gin.Context) {
 	name := c.Query("name")
 	password := c.Query("password")
-	model.UsersCollection.InsertOne(context.Background(), bson.M{"name": name, "password": utils.MD5(password)})
+	var user Person
+	user.Id = primitive.NewObjectID()
+	user.Name = name
+	user.Password = utils.MD5(password)
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = time.Now()
 
+	model.UsersCollection.InsertOne(context.Background(), user)
+	//model.UsersCollection.update({})
 	c.JSON(http.StatusOK, gin.H{
 		"success": true,
 		"msg":     "注册成功",
